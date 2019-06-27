76273819e609fc42bf78404b03b9aff719f25f4a
Johan Benschop <johan.benschop@indivirtual.com>
2019-06-27 23:26:57 +0200

    Added solution with project and a single benchmark

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var summary = BenchmarkRunner.Run<EnumToInt>();
+        }
+    }
+
+    public class EnumToInt
+    {
+        private static readonly Enum foo = TestEnum.Three;
+
+        [Benchmark]
+        public void T1()
+        {
+            int i = Convert.ToInt32(foo);
+        }
+
+        [Benchmark]
+        public void T2()
+        {
+            int i = (int)(object)foo;
+        }
+
+        [Benchmark]
+        public void T3()
+        {
+            int i = (int)Enum.Parse(foo.GetType(), foo.ToString());
+        }
+
+        [Benchmark]
+        public void T4()
+        {
+            int i = (int)Enum.ToObject(foo.GetType(), foo);
+        }
+    }
+
+    public enum TestEnum
+    {
+        One = 1,
+        Two = 2,
+        Three = 3
+    }
+}
