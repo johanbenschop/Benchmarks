d825dedd2acb15c66c02c94e398c652fbf958ea4
Johan Benschop <johan.benschop@indivirtual.com>
2019-07-07 21:45:57 +0200

    Now using BenchmarkSwitcher & new benchmark

---
+++ EnumToInt.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks
+{
+    public class EnumToInt
+    {
+        private static readonly Enum Foo = TestEnum.Three;
+
+        [Benchmark]
+        public void T1()
+        {
+            int i = Convert.ToInt32(Foo);
+        }
+
+        [Benchmark]
+        public void T2()
+        {
+            int i = (int)(object)Foo;
+        }
+
+        [Benchmark]
+        public void T3()
+        {
+            int i = (int)Enum.Parse(Foo.GetType(), Foo.ToString());
+        }
+
+        [Benchmark]
+        public void T4()
+        {
+            int i = (int)Enum.ToObject(Foo.GetType(), Foo);
+        }
+
+        public enum TestEnum
+        {
+            One = 1,
+            Two = 2,
+            Three = 3
+        }
+    }
+}
---
+++ FirstCharToUpper.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks
+{
+    public class FirstCharToUpper
+    {
+        private const string Input = "donderdag 21 april 2017";
+
+        [Benchmark]
+        public string FirstCharToUpper1()
+        {
+            return char.ToUpper(Input[0]) + Input.Substring(1);
+        }
+
+        [Benchmark]
+        public string FirstCharToUpper2()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append(char.ToUpper(Input[0]));
+            stringBuilder.Append(Input.Substring(1));
+
+            return stringBuilder.ToString();
+        }
+
+        [Benchmark]
+        public string FirstCharToUpper3()
+        {
+            var sb = new StringBuilder(Input);
+            sb[0] = char.ToUpper(sb[0]);
+
+            return sb.ToString();
+        }
+
+        [Benchmark]
+        public string FirstCharToUpper4()
+        {
+            var output = Input.ToCharArray();
+            output[0] = char.ToUpper(Input[0]);
+
+            return output.ToString();
+        }
+    }
+}
